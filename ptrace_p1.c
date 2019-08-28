003e0f8a3e2ee063480c20277f80fdd8fc307ffe
Naaz Sibia <sibianjali@gmail.com>
2019-08-28 14:57:55 -0400

    Change code to check args

--- ptrace_p1.c
+++ ptrace_p1.c
@@ -1,6 +1,15 @@
+/*
+Notes: 
+-  by using PTRACE_PEEKUSER as the first argument, we can examine the contents of the USER area where register contents and other 
+information is stored
+
+*/
+#include <sys/ptrace.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <sys/reg.h>/* For constants
+#include <sys/reg.h>
+#include <stdio.h>/* For constants
  ORIG_EAX etc */
 int main(){
     pid_t child;
@@ -11,11 +20,28 @@ int main(){
         execl("/bin/ls", "ls", NULL);
     }
     else {
-        int status;
-        int child_pid = waitpid(-1, &status, __WALL)
-        orig_rax = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * ORIG_RAX, NULL);
-        printf("The child (%d) made a system call %ld\n", child_pid, orig_rax);
-        ptrace(PTRACE_CONT, child, NULL, NULL);
-    }
+        else {
+            while(1) {
+                wait(&status);
+                if(WIFEXITED(status))
+                    break;
+                orig_rax = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * ORIG_RAX, NULL);
+                if(orig_rax == SYS_write) {
+                    if(insyscall == 0) {
+                    /* Syscall entry */
+                    insyscall = 1;
+                    params[0] = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * RDI, NULL);
+                    params[1] = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * RSI, NULL);
+                    params[2] = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * RDX, NULL);
+                    printf("Write called with " "%ld, %ld, %ld\n", params[0], params[1], params[2]);
+                    }
+                    else { /* Syscall exit */
+                        rax = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * RAX, NULL);
+                        printf("Write returned with %ld\n", rax);
+                        insyscall = 0;
+                    }
+                }
+                ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+            }
     return 0;
 }
